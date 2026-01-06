61c10824b1af421b9a58dd7cda6cb8447b73f5da
zurreyab <ahmad.zurreyab@gmail.com>
2026-01-06 13:30:57 +0530

    Update UserController.java
    ok

--- HolidayHomeBooking/src/main/java/com/holidaybookingproject/HolidayHomeBooking/controller/UserController.java
+++ HolidayHomeBooking/src/main/java/com/holidaybookingproject/HolidayHomeBooking/controller/UserController.java
@@ -69,7 +69,7 @@ public class UserController {
 
         User user =new  User();
         user.setId(2l);
-        user.setUsername("jjjjjjjjjjjjjvi use===nnnnn====");
+        user.setUsername("jahmad");
 
 
         return new ResponseEntity<>(user, HttpStatus.OK);
